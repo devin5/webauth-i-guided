afa9ea3cecb027a3cdecde30b41a4ea6e483fb2c
Devin Dias <devindias.tech@gmail.com>
2019-11-11 11:36:04 -0600

    completed

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -9,5 +10,17 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post("/hash", (req, res) => {
+  // read a password from the body
+  // hash the password using bcryptjs
+  // return it to the user in an object that looks like
+  // { password: 'original passsword', hash: 'hashed password' }
+  const password = req.body.password;
+  const hash = bcrypt.hashSync(password, 12);
+  res.status(200).json({ password, hash });
+});
+
+
 
 module.exports = router;
